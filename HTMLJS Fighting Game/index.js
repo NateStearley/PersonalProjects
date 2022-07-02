b7a195558398b247a6e89bcfc3dbcf5a1f395456
stear137 <stear137@umn.edu>
2022-07-02 13:43:25 -0500

    Resolved movement issues and added key listeners for player 2

--- HTMLJS Fighting Game/index.js
+++ HTMLJS Fighting Game/index.js
@@ -33,6 +33,7 @@ class Sprite {
     update() {
         this.draw()
         this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
         
         // If the player is about to hit the bottom stop the sprite
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -56,19 +57,116 @@ const enemy = new Sprite({
 
 console.log(player)
 
+
+// This const represents possible pressed keys in order to more effectivly handle accurate player input
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    },
+    ArrowDown: {
+        pressed: false
+    }
+}
+let lastKey
+
+// Animate Function
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = "black"
-    c.fillRect(0, 0, canvas.width, canvas.height)
+    c.fillRect(0, 0, canvas.width, canvas.height) // Background of arena
     player.update()
     enemy.update()
 
+    // Trigger velocity changes when keys are pressed
+    player.velocity.x = 0
+    if (keys.a.pressed && lastKey == "a") {
+        player.velocity.x = -1
+    } else if (keys.d.pressed && lastKey == "d") {
+       player.velocity.x = 1
+    }
 }
 
 animate()
 
-
 // Event Listeners
+// Key Down
 window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "w":
+            keys.w.pressed = true
+            lastKey = "w"
+            break
+        case "a":
+            keys.a.pressed = true
+            lastKey = "a"
+            break
+        case "s":
+            keys.s.pressed = true
+            lastKey = "s"
+            break
+        case "d":
+            keys.d.pressed = true
+            lastKey = "d"
+            break
+
+        // Arrow Keys - For player 2
+        case "ArrowLeft":
+            keys.ArrowLeft.pressed = true
+            lastKey = "ArrowLeft"
+            break
+        case "ArrowRight":
+            keys.ArrowRight.pressed = true
+            lastKey = "ArrowRight"
+            break
+        case "ArrowUp":
+            keys.ArrowUp.pressed = true
+            lastKey = "ArrowUp"
+            break
+        case "ArrowDown":
+            keys.ArrowDown.pressed = true
+            lastKey = "ArrowDown"
+        
+    }
     console.log(event)
 })
+
+// Key Up
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case "w":
+            keys.w.pressed = false
+            break
+        case "a":
+            keys.a.pressed = false
+            break
+        case "s":
+            keys.s.pressed = false
+            break
+        case 'd':
+            keys.d.pressed = false
+            break
+        
+    }
+    console.log(event)
+})
+
+
+
