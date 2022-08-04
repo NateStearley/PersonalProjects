8f58f79e1f736b7d23fffad19c3f5a31d8c607f4
Nate Stearley <stear137@umn.edu>
2022-08-04 16:30:18 -0500

    First user interface concept

--- RunningMetronome/RunningMetronome WatchKit Extension/ContentView.swift
+++ RunningMetronome/RunningMetronome WatchKit Extension/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        
+        ZStack {
+            
+
+            Circle().frame(width: 200.0)
+            
+            Text("140").multilineTextAlignment(.center).padding().font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).foregroundColor(.black)
+            
+            HStack {
+                
+                // Possible left side buttons
+//                VStack {
+//                    Circle().foregroundColor(.green).frame(width: 30, height: 30).padding(2.0)
+//
+//                    Spacer()
+//
+//                    Circle().foregroundColor(.red).frame(width: 30, height: 30).padding(2.0)
+//
+//                }
+                
+                Spacer()
+                
+                VStack {
+                    ZStack {
+                        Circle().foregroundColor(.green).frame(width: 30, height: 30).padding(2.0)
+                        
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    Circle().foregroundColor(.red).frame(width: 30, height: 30).padding(2.0)
+                        
+                }
+            }
+        }
     }
 }
 
