90808ba90deaf2ed52006ebd8e1dc2c5582753fd
Nate Stearley <stear137@umn.edu>
2022-08-04 16:23:53 -0500

    Boiler plate and first design for apple watch app

---
+++ RunningMetronome/RunningMetronome WatchKit Extension/RunningMetronomeApp.swift
@@ -0,0 +1,19 @@
+//
+//  RunningMetronomeApp.swift
+//  RunningMetronome WatchKit Extension
+//
+//  Created by Nate Stearley on 8/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RunningMetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
